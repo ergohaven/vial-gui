24201810cf0bcb8b83dac22294cd8658c9ec5a38
Danil Tolkachev <dandur.oak@gmail.com>
2025-11-08 11:47:54 +0300

    fix

--- src/main/python/editor/kbd_settings.py
+++ src/main/python/editor/kbd_settings.py
@@ -8,11 +8,12 @@ class KbdSettings(QmkSettings):
 
     def rebuild(self, device):
         super(QmkSettings, self).rebuild(device)
-        if self.valid():
-            self.keyboard = device.keyboard
-            self.settings_set = self.keyboard.qmk_settings.kbd_settings_set
+        self.keyboard = device.keyboard
+        if self.keyboard:
             self.qmk_settings = self.keyboard.qmk_settings
-            self.reload_settings()
+            self.settings_set = self.keyboard.qmk_settings.kbd_settings_set
+            if self.valid():
+                self.reload_settings()
 
     def valid(self):
         return (isinstance(self.device, VialKeyboard) and
